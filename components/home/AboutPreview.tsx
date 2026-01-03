000e96e10c4f200e7d93aa23e65e0a0728e24208
Liam Y Lehr <liamlylehr@gmail.com>
2026-01-03 18:34:00 -0500

    fixes to the metadata

--- components/home/AboutPreview.tsx
+++ components/home/AboutPreview.tsx
@@ -11,12 +11,15 @@ export default function AboutPreview() {
       <Accordion
         type="single"
         collapsible
-        className="mt-8 sm:mt-16 w-full max-w-2xl animate-fade-in-left"
-        style={{
-          animationDelay: "300ms",
-        }}
+        className="mt-8 sm:mt-16 w-full max-w-2xl"
       >
-        <AccordionItem value="about-me">
+        <AccordionItem
+          value="about-me"
+          className="animate-fade-in-left"
+          style={{
+            animationDelay: "300ms",
+          }}
+        >
           <AccordionTrigger>About Me</AccordionTrigger>
           <AccordionContent>
             <p>
@@ -26,7 +29,13 @@ export default function AboutPreview() {
             </p>
           </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="my-skills">
+        <AccordionItem
+          value="my-skills"
+          className="animate-fade-in-left"
+          style={{
+            animationDelay: "600ms",
+          }}
+        >
           <AccordionTrigger>My Skills</AccordionTrigger>
           <AccordionContent>
             <ul className="list-disc list-inside">
@@ -36,6 +45,36 @@ export default function AboutPreview() {
               <li>Character Development</li>
               <li>Storytelling</li>
             </ul>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem
+          value="contact-me"
+          className="animate-fade-in-left"
+          style={{
+            animationDelay: "900ms",
+          }}
+        >
+          <AccordionTrigger>Contact Me</AccordionTrigger>
+          <AccordionContent>
+            <p>
+              Feel free to reach out to me via email at{" "}
+              <a
+                href="mailto:liamlylehr@gmail.com"
+                className="text-blue-500 underline"
+              >
+                liamlylehr at gmail dot com
+              </a>{" "}
+              or connect with me on{" "}
+              <a
+                href="https://www.linkedin.com/in/liam-lehr/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline"
+              >
+                LinkedIn
+              </a>
+              .
+            </p>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
